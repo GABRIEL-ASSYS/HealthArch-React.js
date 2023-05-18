7d5fab404e4ae8f94c36d17654a64e7280224994
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-05-18 12:04:36 -0300

    adicionando useState para tipoUsuario

--- src/components/CadastroUsuario/index.js
+++ src/components/CadastroUsuario/index.js
@@ -2,107 +2,116 @@ import BotaoPadrao from 'components/BotaoPadrao'
 import styles from './CadastroUsuario.module.css'
 
 export default function CadastroUsuario() {
+  const [tipoCadastro, setTipoCadastro] = useState('Cliente')
+
   return (
     <div className={styles.containerCadastros}>
 
-      <select className={styles.selectCadastro}>
+      <select
+        className={styles.selectCadastro}
+        value={tipoCadastro}
+        onChange={(tipoCadastro) => setTipoCadastro(tipoCadastro.target.value)}
+      >
         <option
           className={styles.optionCadastro}
-          value={Cliente}
+          value='Cliente'
         >Cliente</option>
         <option
           className={styles.optionCadastro}
-          value={Profissional}
-          selected
+          value='Profissional'
         >Profissional</option>
       </select>
 
-      <div className={styles.cadastro}>
-        <h2 className={styles.tituloCadastro}>Cadastrar como Cliente</h2>
-        <label className={styles.labelCadastro}>Nome:</label>
-        <input
-          className={styles.inputCadastro}
-          placeholder='Digite seu nome'
-          value={nomeCliente}
-          onChange={(nomeCliente) => setNomeCliente(nomeCliente.target.value)}
-        />
-        <br />
-        <label className={styles.labelCadastro}>Email:</label>
-        <input
-          className={styles.inputCadastro}
-          placeholder='Digite seu email'
-          type='email'
-          value={emailCliente}
-          onChange={(emailCliente) => setEmailCliente(emailCliente.target.value)}
-        />
-        <br />
-        <label className={styles.labelCadastro}>Senha:</label>
-        <input
-          className={styles.inputCadastro}
-          placeholder='Digite sua senha'
-          type='password'
-          value={passwordCliente}
-          onChange={(passwordCliente) => setPasswordCliente(passwordCliente.target.value)}
-        />
-        <br />
-        <label className={styles.labelCadastro}>Telefone:</label>
-        <input
-          className={styles.inputCadastro}
-          placeholder='Digite seu numero de telefone'
-          type='tel'
-          value={telefoneCliente}
-          onChange={(telefoneCliente) => setTelefoneCliente(telefoneCliente.target.value)}
-        />
+      {tipoCadastro === 'Cliente' && (
+        <div className={styles.cadastro}>
+          <h2 className={styles.tituloCadastro}>Cadastrar como Cliente</h2>
+          <label className={styles.labelCadastro}>Nome:</label>
+          <input
+            className={styles.inputCadastro}
+            placeholder='Digite seu nome'
+            value={nomeCliente}
+            onChange={(nomeCliente) => setNomeCliente(nomeCliente.target.value)}
+          />
+          <br />
+          <label className={styles.labelCadastro}>Email:</label>
+          <input
+            className={styles.inputCadastro}
+            placeholder='Digite seu email'
+            type='email'
+            value={emailCliente}
+            onChange={(emailCliente) => setEmailCliente(emailCliente.target.value)}
+          />
+          <br />
+          <label className={styles.labelCadastro}>Senha:</label>
+          <input
+            className={styles.inputCadastro}
+            placeholder='Digite sua senha'
+            type='password'
+            value={passwordCliente}
+            onChange={(passwordCliente) => setPasswordCliente(passwordCliente.target.value)}
+          />
+          <br />
+          <label className={styles.labelCadastro}>Telefone:</label>
+          <input
+            className={styles.inputCadastro}
+            placeholder='Digite seu numero de telefone'
+            type='tel'
+            value={telefoneCliente}
+            onChange={(telefoneCliente) => setTelefoneCliente(telefoneCliente.target.value)}
+          />
 
-        <BotaoPadrao texto='Cadastrar' onClick={novoUsuarioCliente} />
-      </div>
-      <div className={styles.cadastro}>
-        <h2 className={styles.tituloCadastro}>Cadastrar como Profissional</h2>
-        <label className={styles.labelCadastro}>Nome:</label>
-        <input
-          className={styles.inputCadastro}
-          placeholder='Digite seu nome'
-          value={nomeProfissional}
-          onChange={(nomeProfissional) => setNomeProfissional(nomeProfissional.target.value)}
-        />
-        <br />
-        <label className={styles.labelCadastro}>Email:</label>
-        <input
-          className={styles.inputCadastro}
-          placeholder='Digite seu email'
-          type='email'
-          value={emailProfissional}
-          onChange={(emailProfissional) => setEmailProfissional(emailProfissional.target.value)}
-        />
-        <br />
-        <label className={styles.labelCadastro}>Senha:</label>
-        <input
-          className={styles.inputCadastro}
-          placeholder='Digite sua senha'
-          type='password'
-          value={senhaProfissional}
-          onChange={(senhaProfissional) => setSenhaProfissional(senhaProfissional.target.value)}
-        />
-        <br />
-        <label className={styles.labelCadastro}>Telefone:</label>
-        <input
-          className={styles.inputCadastro}
-          placeholder='Digite seu numero de telefone'
-          type='tel'
-          value={telefoneProfissional}
-          onChange={(telefoneProfissional) => setTelefoneProfissional(telefoneProfissional.target.value)} 
-        />
-        <br />
-        <label className={styles.labelCadastro}>Código Regional:</label>
-        <input
-          className={styles.inputCadastro}
-          placeholder='Digite seu código regional'
-          value={codigoRegional}
-          onChange={(codigoRegional) => setCodigoRegional(codigoRegional.target.value)}
-        />
+          <BotaoPadrao texto='Cadastrar' onClick={novoUsuarioCliente} />
+        </div>
+      )}
+      {tipoCadastro === 'Profissional' && (
+        <div className={styles.cadastro}>
+          <h2 className={styles.tituloCadastro}>Cadastrar como Profissional</h2>
+          <label className={styles.labelCadastro}>Nome:</label>
+          <input
+            className={styles.inputCadastro}
+            placeholder='Digite seu nome'
+            value={nomeProfissional}
+            onChange={(nomeProfissional) => setNomeProfissional(nomeProfissional.target.value)}
+          />
+          <br />
+          <label className={styles.labelCadastro}>Email:</label>
+          <input
+            className={styles.inputCadastro}
+            placeholder='Digite seu email'
+            type='email'
+            value={emailProfissional}
+            onChange={(emailProfissional) => setEmailProfissional(emailProfissional.target.value)}
+          />
+          <br />
+          <label className={styles.labelCadastro}>Senha:</label>
+          <input
+            className={styles.inputCadastro}
+            placeholder='Digite sua senha'
+            type='password'
+            value={senhaProfissional}
+            onChange={(senhaProfissional) => setSenhaProfissional(senhaProfissional.target.value)}
+          />
+          <br />
+          <label className={styles.labelCadastro}>Telefone:</label>
+          <input
+            className={styles.inputCadastro}
+            placeholder='Digite seu numero de telefone'
+            type='tel'
+            value={telefoneProfissional}
+            onChange={(telefoneProfissional) => setTelefoneProfissional(telefoneProfissional.target.value)}
+          />
+          <br />
+          <label className={styles.labelCadastro}>Código Regional:</label>
+          <input
+            className={styles.inputCadastro}
+            placeholder='Digite seu código regional'
+            value={codigoRegional}
+            onChange={(codigoRegional) => setCodigoRegional(codigoRegional.target.value)}
+          />
 
-        <BotaoPadrao texto='Cadastrar' onClick={novoUsuarioCliente}/>
-      </div>
+          <BotaoPadrao texto='Cadastrar' onClick={novoUsuarioCliente} />
+        </div>
+      )}
     </div>
   )
 }
